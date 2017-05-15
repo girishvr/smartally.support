ba39a9d727c2f059348633560831eb12a6d4b1c3
Muqtadir Ahmed <muqtadir007@gmail.com>
2017-05-15 15:29:17 +0530

    Added api endpoint.

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'));
 // Set the app secret key.
 app.set('secretKey', config.secret);
 // Add routes.
-app.use('/', routes);
+app.use('/api/', routes);
 
 // DB connection.
 mongoose.Promise = global.Promise;
